7bad2842e64cdba33c02ebd8915a0f30c031d1e8
ansardebox <sanaul.ansari@deboxglobal.com>
2024-05-30 17:20:18 +0530

    updated

--- src/Pages/query/Summary.jsx
+++ src/Pages/query/Summary.jsx
@@ -1,11 +1,353 @@
 import React from "react";
 
-const Summary = () =>{
-    return(
-        <>
-            <h1 className="p-2">THIS IS SUMMARY</h1>
-        </>
-    )
+const Summary = () => {
+  return (
+    <>
+      <div className="container-fluid border">
+        <div className="row">
+          <div className="col-2 border-right d-flex justify-content-center align-items-center">
+            <div>
+              <p className="m-0">Day 1 | Delhi</p>
+              <p className="m-0">05 Apr 24 | Fri</p>
+            </div>
+          </div>
+          <div className="col-10">
+            <div className="row px-1">
+              <div className="col-4 d-flex align-items-center p-0">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Arrival Flight
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\arrival.png"
+                      alt=""
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Monument
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\monument.png"
+                      alt="monument"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">Hotel</p>
+                    <img
+                      src="\public\global_assets\images\qoutation\hotel.png"
+                      alt="hotel"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row px-1">
+              <div className="col-4 d-flex align-items-center p-0">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Departure Flight
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\takeoff.png"
+                      alt=""
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Activity
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\activity.png"
+                      alt="monument"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Restaurant
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\restaurant.png"
+                      alt="hotel"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row px-1">
+              <div className="col-4 d-flex align-items-center p-0">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Guide
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\guide.png"
+                      alt=""
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row border-top">
+          <div className="col-2 border-right d-flex justify-content-center align-items-center">
+            <div>
+              <p className="m-0">Day 1 | Delhi</p>
+              <p className="m-0">05 Apr 24 | Fri</p>
+            </div>
+          </div>
+          <div className="col-10">
+            <div className="row px-1">
+              <div className="col-4 d-flex align-items-end p-0">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Arriving Flight
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\arrival.png"
+                      alt=""
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Monument
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\monument.png"
+                      alt="monument"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">Hotel</p>
+                    <img
+                      src="\public\global_assets\images\qoutation\hotel.png"
+                      alt="hotel"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row border-top">
+          <div className="col-2 border-right d-flex justify-content-center align-items-center">
+            <div>
+              <p className="m-0">Day 1 | Delhi</p>
+              <p className="m-0">05 Apr 24 | Fri</p>
+            </div>
+          </div>
+          <div className="col-10">
+            <div className="row px-1">
+              <div className="col-4 d-flex align-items-center p-0">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Arrival Flight
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\arrival.png"
+                      alt=""
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-9 font-weight-bold">
+                      Monument
+                    </p>
+                    <img
+                      src="\public\global_assets\images\qoutation\monument.png"
+                      alt="monument"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air
+                      India), Flight Class
+                      <span className="font-weight-bold">First Class</span>
+                      ,Destination
+                      <span className="font-weight-bold">Maldives</span> to
+                      <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="row py-2">
+                  <div className="col-3 p-0 d-flex flex-column align-items-center">
+                    <p className="m-0 font-size-10 font-weight-bold">Hotel</p>
+                    <img
+                      src="\public\global_assets\images\qoutation\hotel.png"
+                      alt="hotel"
+                      style={{ height: "20px", width: "20px" }}
+                    />
+                  </div>
+                  <div className="col-9 p-0">
+                    <p className="font-size-10">
+                      <span className="font-weight-bold">15226</span> (Air India), Flight Class <span className="font-weight-bold">First Class</span>,Destination <span className="font-weight-bold">Maldives</span> to <span className="font-weight-bold">Mumbai</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Summary;
