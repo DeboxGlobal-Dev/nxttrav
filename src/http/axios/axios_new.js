16e4f4841324bcde297acec83cc3f7ccc2be1804
ansardebox <sanaul.ansari@deboxglobal.com>
2024-10-04 10:01:18 +0530

    UPDATED

--- src/http/axios/axios_new.js
+++ src/http/axios/axios_new.js
@@ -7,7 +7,23 @@ const axiosNew = axios.create({
   },
 });
 
+
+// run others on 8003 port 
 const axiosOther = axios.create({
+  baseURL: "http://127.0.0.1:8003/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const axiosHotel = axios.create({
+  baseURL: "http://20.198.9.47/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const axiosGuide = axios.create({
   baseURL: "http://20.198.9.47/api/",
   headers: {
     "Content-Type": "application/json",
